337e7fa280d46e2d20635586f50468c0454c40b4
Thibaud <thibaud.delabr@gmail.com>
2016-04-18 16:44:11 +0200

    Ajout : aside panier, contact et recherche 	background 	carousel

--- application/views/liens/accueil.php
+++ application/views/liens/accueil.php
@@ -1,54 +1,116 @@
-<header>
-	<img src="<?=base_url("assets/img/logo.png")?>" alt="Logo village green">
-</header>
-
-<nav id="navGreen" class="navbar navbar-default">
-	<div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-              <li><a href="#">Rubrique</a></li>
-              <li><a href="#">Rubrique</a></li>
-              <li><a href="#">Rubrique</a></li>
-              <li><a href="#">Rubrique</a></li>
-              <li><a href="#">Rubrique</a></li>
-              <li><a href="#">Rubrique</a></li>
-            </ul class="nav navbar-nav navbar-right"> 
-              <li><a href="#">Espace Client</a></li>
-            </ul>      
-        </div>
-   </div>
-</nav>
-<aside>
-	
-</aside>
-<section>
-	
-</section>
-<footer>
-	<header>
-		
+<div class="container">
+	<header class="row">
+		<div class="col-md-offset-1 col-md-8">
+			<img class="center-block" src="<?=base_url("assets/img/logo.png")?>" alt="Logo village green">
+		</div>
+		<aside id="asideHeader" class="col-md-2">
+			<a href="#" class="text-center"><div>Panier <span class="glyphicon glyphicon-shopping-cart"></span></div></a>
+			<a href="#" class="text-center"><div id="mid">Contact  <span class="glyphicon glyphicon-envelope"></span></div></a>
+			<a href="#"><div class="text-center"><input id="searchAside" type="search" class="input-sm form-control" placeholder="Recherche"><span class="glyphicon glyphicon-search"></span></div></a>   			
+		</aside>
 	</header>
-	<section>
-		
-	</section>
-	<section>
-		
-	</section>
-	<section>
-		
-	</section>
-	<section>
-		
-	</section>
+	<div class="row">
+		<div id="divNav" class="col-md-offset-1 col-md-10">
+			<nav id="navGreen" class="navbar navbar-default">
+				<div class="container-fluid">
+			        <div class="navbar-header">
+			            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+			              <span class="sr-only">Toggle navigation</span>
+			              <span class="icon-bar"></span>
+			              <span class="icon-bar"></span>
+			              <span class="icon-bar"></span>
+			            </button>
+			        </div>
+			        <div id="navbar" class="navbar-collapse collapse">
+			            <ul class="nav navbar-nav">
+			              <li><a href="#">Rubrique</a></li>
+			              <li><a href="#">Rubrique</a></li>
+			              <li><a href="#">Rubrique</a></li>
+			              <li><a href="#">Rubrique</a></li>
+			              <li><a href="#">Rubrique</a></li>
+			              <li><a href="#">Rubrique</a></li>
+			            </ul>
+			            <ul class="nav navbar-nav navbar-right"> 
+			              <li><a href="#">Espace Client</a></li>
+			            </ul>      
+			        </div>
+			   </div>
+			</nav>
+		</div>
+	</div>
+		<div class="row">
+			<section id="corpsSection" class="col-md-offset-1 col-md-10">	
+				<div class="row">
+					<div id="divCar" class="col-md-offset-1 col-md-10">
+						<div id="myCarousel" class="carousel slide" data-ride="carousel">
+						    <ol class="carousel-indicators">
+							    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+							    <li data-target="#myCarousel" data-slide-to="1"></li>
+							    <li data-target="#myCarousel" data-slide-to="2"></li>
+						    </ol>
+						    <div class="carousel-inner" role="listbox">
+							    <div class="item active">
+								    <img class="first-slide" src="<?=base_url("assets/img/visu-slider.png")?>" alt="First slide">	
+								    <div class="carousel-caption">
+								    <h1>Lorem Ipsum</h1>
+								    <p>Lorem Ipsum</p>
+								    </div>					        
+							    </div>
+							    <div class="item">
+								    <img class="first-slide" src="<?=base_url("assets/img/visu-slider.png")?>" alt="Second slide">
+								    <div class="carousel-caption">
+								    <h1>Lorem Ipsum</h1>
+								    <p>Lorem Ipsum</p>
+								    </div>						         
+							    </div>
+							    <div class="item">
+								    <img class="first-slide" src="<?=base_url("assets/img/visu-slider.png")?>" alt="Third slide">	
+								    <div class="carousel-caption">
+								    <h1>Lorem Ipsum</h1>
+								    <p>Lorem Ipsum</p>
+								    </div>					          
+							    </div>						            
+							    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+							    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+							    <span class="sr-only">Previous</span>
+							    </a>
+							    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+							    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+							    <span class="sr-only">Next</span>
+							    </a>
+    						</div>
+    						<hr />
+						</div>					
+					</div>
+				</div>
+				<div class="row">
+					<div id="row2" class="col-md-offset-1 col-md-10">
+						<img src="<?=base_url("assets/img/nouveautés.png")?>">
+						<img src="<?=base_url("assets/img/promotions.png")?>">
+						<img src="<?=base_url("assets/img/meilleures ventes.png")?>">
+						<img src="<?=base_url("assets/img/produits du moment.png")?>">
+					</div>
+				</div>
+			</section>
+		</div>
 	<footer>
-		
+		<header>
+			
+		</header>
+		<section>
+			
+		</section>
+		<section>
+			
+		</section>
+		<section>
+			
+		</section>
+		<section>
+			
+		</section>
+		<footer>
+			
+		</footer>
 	</footer>
-</footer>
+</div>
